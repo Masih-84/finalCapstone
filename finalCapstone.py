102ca112bc4c40e17f3e614535f5314a899ee746
Masih <masih.py@gmail.com>
2023-06-19 16:20:20 +0100

    added instruction in second line

--- finalCapstone.py
+++ finalCapstone.py
@@ -1,4 +1,10 @@
-"""Functions for creating, transforming, and adding prefixes to strings."""
+"""Functions for creating, transforming, and adding prefixes to strings.
+Instructions
+You are helping your younger sister with her English vocabulary homework,
+which she's finding very tedious. Her class is learning to create new words by adding prefixes and suffixes.
+Given a set of words, the teacher is looking for correctly transformed words with correct spelling by
+adding the prefix to the beginning or the suffix to the ending.
+"""
 def add_prefix_un(word):
     """Take the given word and add the 'un' prefix.
 
