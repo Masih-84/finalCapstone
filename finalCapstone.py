be6a3c09960a0d6a91edc93960a074d31541a386
Masih <masih.py@gmail.com>
2023-06-19 16:02:00 +0100

    added last code change adjective to verb

--- finalCapstone.py
+++ finalCapstone.py
@@ -42,3 +42,16 @@ def remove_suffix_ness(word):
         return word[:-4]
     pass
 
+def adjective_to_verb(sentence, index):
+    """Change the adjective within the sentence to a verb.
+
+    :param sentence: str - that uses the word in sentence.
+    :param index: int - index of the word to remove and transform.
+    :return: str - word that changes the extracted adjective to a verb.
+
+    For example, ("It got dark as the sun set", 2) becomes "darken".
+    """
+    words = sentence.split()
+    adjective = words[index].strip('.,!?')
+    return adjective + 'en'
+    pass
